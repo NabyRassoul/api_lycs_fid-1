9310d71edd5b0dab89e0449059bbd81632299f03
NabyRassoul <nabipulo@gmail.com>
2024-01-08 12:08:34 +0000

    filefound

--- lycsfid/local_settings.py
+++ lycsfid/local_settings.py
@@ -1,3 +1,3 @@
 ALLOWED_HOSTS =[]
 DEBUG = True
-cred= "C:/Users/HP/OneDrive/Bureau/Lycs_fidilisation/Fid_BackEnd/credentials.json"
+cred= "C:\Users\HP\OneDrive\Bureau\Lycs_fidilisation\Fid_BackEnd\credentials.json"
